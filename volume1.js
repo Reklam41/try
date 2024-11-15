8aaa91d023c7458faebd67e82bd230f1a8948621
Reklam41 <bonganebongza2002@gmail.com>
2024-11-15 19:59:37 +0000

    the hamburger is messed up

--- volume1.js
+++ volume1.js
@@ -36,3 +36,12 @@ document.addEventListener("mousemove", (e) => {
   book.style.transform = `rotateY(${currentRotation}deg) rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
   */
 });
+
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
